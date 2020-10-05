98765c9bd5aee766888660aa01b953d76ae34b15
Daniel <melolingd@gmail.com>
2020-10-04 18:49:02 -0700

    added updated comments and variable consistancy.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,33 +1,39 @@
 package cse360assignment02;
 
 public class AddingMachine {
-	 private int total;
-	 private String history = "0 ";
+	 private int total;			// Declare private variable total
+	 private String history;	// Declare private variable history
 	  
 	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
+	    total = 0;  		// set total to base case
+	    history = "0 ";		// set history to base case
 	  }
 	  
+	  // Returns variable total
 	  public int getTotal () {
-	    return total;
+	    return total;	// Returns total to method call
 	  }
 	  
+	  // Adds parameter value to total
 	  public void add (int value) {
-		  total = total + value;
-		  history += "+ " + value + " ";
+		  total = total + value;			// Adds value to total and save change in total
+		  history += "+ " + value + " ";	// Updates history of known addition
 	  }
-
+	  
+	  // Subtracts parameter value from total
 	  public void subtract (int value) {
-		  total = total - value;
-		  history += "- " + value + " ";
+		  total = total - value;			// Subtracts value from total and stores change in total
+		  history += "- " + value + " ";	// Updates history of known subtraction
 	  }
-
+	  
+	  // Returns value of changes to total
 	  public String toString () {
-	    return history;
+	    return history;		// returns variable history to method call
 	  }
-
+	  
+	  // Clears your "memory" values
 	  public void clear() {
-		  history = "0 ";
-		  total = 0;
+		  history = "0 ";	// Sets history value to base case "0 "
+		  total = 0;		// Sets total to base case 0
 	  }
 }
